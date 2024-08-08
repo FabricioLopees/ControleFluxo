09a9f9f86514e9a711b854612a456cfbf8d2b95f
FabricioLopees <prog.fabriciolopes@gmail.com>
2024-08-08 12:45:16 -0300

    feat: add exception handling
    - add ParametrosInvalidosException to verify if the range of numbers
      it's correct
    
    - add InputMismatchException to handling invalid types

--- src/edu/fabricio/app/Contador.java
+++ src/edu/fabricio/app/Contador.java
@@ -1,23 +1,40 @@
 package edu.fabricio.app;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Contador{
-	public static void main(String[] args) {
-	
-		Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) throws ParametrosInvalidosException {
+		try {
+			// Obtém os números do usuário
+			
+			Scanner scan = new Scanner(System.in);
 
-		System.out.println("Digite um número:");
-		int primeiroNumero = scan.nextInt();
+			System.out.println("Digite um número:");
+			int primeiroNumero = scan.nextInt();
+			
+			System.out.println("Digite outro número:");
+			int segundoNumero = scan.nextInt();
+			
+			scan.close();
+			
+			// Executa teste se o primeiro número é maior que o segundo número
+			
+			if(primeiroNumero > segundoNumero) {
+				throw new ParametrosInvalidosException();
+			}
+		} catch (ParametrosInvalidosException parametrosInvalidos) {
+			parametrosInvalidos.dispararErro();
+		} catch (InputMismatchException e) {
+			System.out.println("Tipo de dado inválido! Somente numéricos do tipo inteiro.");
+			System.out.println("Tente novamente...");
+		}
 		
-		System.out.println("Digite outro número:");
-		int segundoNumero = scan.nextInt();
 		
-		scan.close();
-		
-		
-		// TEST
-		
-		System.out.println("Os número digitados são:"+primeiroNumero+" e "+segundoNumero);
+		/*
+		 * // TEST
+		 * 
+		 * System.out.println("Os número digitados são:"+primeiroNumero+" e "
+		 * +segundoNumero);
+		 */
 	}
-	
 }
